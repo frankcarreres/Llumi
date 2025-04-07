051e768b62b133360ff4e40830deca7feef895b8
AntonioTian <lliurexantonio@gmail.com>
2025-04-07 18:05:05 +0200

    NVL-3 [EVO] * Se modifica la plantilla de Material dashboard

---
+++ frontend/Administracion/src/layouts/tables/data/casosActivosTable.js
@@ -0,0 +1,156 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDBadge from "components/MDBadge";
+
+export default function data() {
+  const Casos = ({ name, email }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDBox>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+        <MDTypography variant="caption">{email}</MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const Tipo = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "Casos", accessor: "casos", width: "30%", align: "left" },
+      { Header: "Tipo de acoso", accessor: "tipos", align: "left" },
+      { Header: "Estado", accessor: "estodo", align: "center" },
+      { Header: "Fecha de incidencia", accessor: "fecha", align: "center" },
+      { Header: "Acciones", accessor: "action", align: "center" },
+    ],
+
+    rows: [
+      {
+        casos: <Casos name="John Michael" email="john@creative-tim.com" />,
+        tipos: <Tipo title="Manager" description="Organization" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="En Proceso" color="info" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            23/04/18
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        casos: <Casos name="Alexa Liras" email="alexa@creative-tim.com" />,
+        tipos: <Tipo title="Programator" description="Developer" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Resuelto" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            11/01/19
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        casos: <Casos name="Laurent Perrier" email="laurent@creative-tim.com" />,
+        tipos: <Tipo title="Executive" description="Projects" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="En proceso" color="info" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            19/09/17
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        casos: <Casos name="Michael Levi" email="michael@creative-tim.com" />,
+        tipos: <Tipo title="Programator" description="Developer" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="En proceso" color="info" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            24/12/08
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        casos: <Casos name="Richard Gran" email="richard@creative-tim.com" />,
+        tipos: <Tipo title="Manager" description="Executive" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="Resuelto" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            04/10/21
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        casos: <Casos name="Miriam Eric" email="miriam@creative-tim.com" />,
+        tipos: <Tipo title="Programator" description="Developer" />,
+        estodo: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="No atentido" color="error" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        fecha: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            14/09/20
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
