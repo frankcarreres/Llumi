31f49c140690893db44bd236ed842fcdbfc04017
Juan Perpiña <jperpi01@gmail.com>
2025-04-22 12:02:59 +0200

    NVL-20[EVO] Merge endPoint ercursos multimedia Frank

--- backend/routes/recursRoutes.js
+++ backend/routes/recursRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { syncNoticias } = require('../controllers/recursController');
-const { getNoticias } = require('../controllers/recursController');
-
+const { syncNoticias, getMultimedia, getNoticias, syncMultimedia } = require('../controllers/recursController');
 // Ruta para sincronizar noticias
 router.get('/syncNoticias', syncNoticias);
 
 // Ruta para obtener las noticias
 router.get('/noticias', getNoticias);
+
+// Ruta para sincronizar los videos
+router.get('/syncMultimedia', syncMultimedia);
+
+// Ruta para obtener las videos
+router.get('/multimedia', getMultimedia);
 
 module.exports = router;
