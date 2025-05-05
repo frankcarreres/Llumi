014ab1339152bf8112d5429c3d206142ea8145c1
Juan Perpiña <jperpi01@gmail.com>
2025-05-05 09:29:36 +0200

    NVL-39[EVO] modificar puntuaciones chat

--- frontend/src/pages/Presentation/components/chat/utils/calculoRiesgo.js
+++ frontend/src/pages/Presentation/components/chat/utils/calculoRiesgo.js
@@ -4,10 +4,10 @@
  * @returns {string} Nivel de riesgo: 'riesgo_critico', 'riesgo_alto', etc.
  */
 export function calcularNivelRiesgo(score) {
-  if (score <= 11 && score >= 0) return "Sin riesgo ";
-  if (score <= 19 && score >= 12) return "Riesgo medio";
-  if (score <= 26 && score >= 20) return "Riesgo alto";
-  if (score <= 33 && score >= 27) return "Riesgo muy alto";
-  if (score >= 34) return "Riesgo crítico";
+  if (score <= 10 && score >= 0) return "Sin riesgo ";
+  if (score <= 13 && score >= 11) return "Riesgo medio";
+  if (score <= 16 && score >= 14) return "Riesgo alto";
+  if (score <= 19 && score >= 17) return "Riesgo muy alto";
+  if (score >= 20) return "Riesgo crítico";
   return "sin_indicios";
 }
