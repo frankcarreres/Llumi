8dc8975cec254a8c52f4f18da6921651a1c740bc
AntonioTian <lliurexantonio@gmail.com>
2025-05-16 17:46:50 +0200

    NVL-80 [EVO] * Se añade breakpoint a la vista del editor

--- frontend/src/routes/adminRoutes.js
+++ frontend/src/routes/adminRoutes.js
@@ -7,7 +7,7 @@ const Tables = lazy(() => import("admin/layouts/tables"));
 const Notifications = lazy(() => import("admin/layouts/notifications"));
 const Profile = lazy(() => import("admin/layouts/profile"));
 const DenunciaPage = lazy(() => import("admin/layouts/denuncias"));
-const CreateNoticia = lazy(() => import("admin/components/MDNoticias/crearNoticiasPage"));
+const CreateNoticia = lazy(() => import("admin/layouts/Recursos"));
 
 export default [
   /* ---------- DASHBOARD (centro) ---------- */
