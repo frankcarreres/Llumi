cb7663a882cab6cfcbf749c69c9951f65c311e98
Juan Perpiña <jperpi01@gmail.com>
2025-06-02 11:02:22 +0200

    Sobreescritura test

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -8,6 +8,6 @@ const verificarToken = require('../middlewares/verificarToken');
 router.post('/', verificarToken, testController.guardarTest);
 router.patch("/:id_test/denuncia", testController.vincularDenuncia);
 router.get("/usuario/me",verificarToken, testController.getTestPorIdUsuario);
-router.put("/test/update",verificarToken, testController.actualizarTest);
+router.put("/update",verificarToken, testController.actualizarTest);
 
 module.exports = router;
